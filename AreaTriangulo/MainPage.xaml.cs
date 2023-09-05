a12a4a9bfaea86257bf1b5e1f672d0ad1a1c947f
Yulissa22 <ec1413220@gmail.com>
2023-09-04 22:25:53 -0600

    Agregar archivos de proyecto.

---
+++ AreaTriangulo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace AreaTriangulo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	private void btncalcular_Clicked(object sender, EventArgs e)
+	{
+		if (!string.IsNullOrEmpty(A.Text) && !string.IsNullOrEmpty(B.Text) &&
+		!string.IsNullOrEmpty(C.Text))
+		{
+			double ladoA, ladoB, ladoC, area, semiperimetro;
+			ladoA = Convert.ToInt32(A.Text);
+			ladoB = Convert.ToInt32(B.Text);
+			ladoC = Convert.ToInt32(C.Text);
+
+			semiperimetro = (ladoA + ladoB + ladoC) /2;
+
+			area = Math.Sqrt(semiperimetro * (semiperimetro - ladoA) * (semiperimetro - ladoB) * (semiperimetro - ladoC));
+
+			Area.Text=area.ToString("N2");
+
+		}
+		else
+		{
+			DisplayAlert("Error", "Ocurrio un error al introducir el dato", "OK");
+		}
+	}
+}
+
